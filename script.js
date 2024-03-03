1eeda2f97dd7ea398ef58bc665ce1853b3d09a7b
w-vinhatto <vvinhatto@hotmail.com>
2024-03-03 17:49:32 -0300

    commit responsive

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const form = document.querySelector(".formulario_name")
+
+function contato() {
+    form.style.right = "50%";
+    form.style.transform = "translateX(50%)"
+}
+
+function escForm() {
+    form.style.right = "-294px";
+    form.style.transform = "translateX(0)"
+}
+
+
 
 const Myobserver = new IntersectionObserver( (entries)=>{
     entries.forEach((entry)=>{
